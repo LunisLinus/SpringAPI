812d70bf6b746c5084cc528524809a7e93db86c5
Sonya <142392876+LunisLinus@users.noreply.github.com>
2025-04-26 10:16:47 +0400

    Init commit

---
+++ src/main/java/org/knit241/Knit241Application.java
@@ -0,0 +1,13 @@
+package org.knit241;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knit241Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Knit241Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/knit241/Knit241ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.knit241;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knit241ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
